b20b87de35879e998c405cc3c5ab89afb454070a
Bagdad <ntorgov@gmail.com>
2016-11-20 19:06:09 +0300

    0.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'akerius:translit',
-	version: '0.0.2',
+	version: '0.0.3',
 	// Brief, one-line summary of the package.
 	summary: 'Transliteration for Meteor',
 	// URL to the Git repository containing the source code for this package.
@@ -11,8 +11,8 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-	//api.versionsFrom('1.0.1');
-	//api.use('ecmascript');
-	api.mainModule('translit.js');
+
+	api.addFiles('translit.js');
+	api.export('translit');
 });
 
