5f7bb4cf5f1badffb992d1ac08c8875c750d29ec
Bagdad <ntorgov@gmail.com>
2016-12-01 17:39:16 +0300

    0.0.3

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # meteor-translit
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/d23feeac3d4e48299aefe14e5dbbce7b)](https://www.codacy.com/app/ntorgov/meteor-translit?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=ntorgov/meteor-translit&amp;utm_campaign=Badge_Grade)
+
 Transliteration for Meteor.
 
 ## Usage
--- package.js
+++ package.js
@@ -1,18 +1,14 @@
 Package.describe({
-	name: 'akerius:translit',
-	version: '0.0.3',
-	// Brief, one-line summary of the package.
-	summary: 'Transliteration for Meteor',
-	// URL to the Git repository containing the source code for this package.
-	git: 'https://github.com/ntorgov/meteor-translit',
-	// By default, Meteor will default to using README.md for documentation.
-	// To avoid submitting documentation, set this field to null.
-	documentation: 'README.md'
+	name: "akerius:translit",
+	version: "0.0.3",
+	summary: "Transliteration for Meteor",
+	git: "https://github.com/ntorgov/meteor-translit",
+	documentation: "README.md"
 });
 
 Package.onUse(function (api) {
 
-	api.addFiles('translit.js');
-	api.export('translit');
+	api.addFiles("translit.js");
+	api.export("translit");
 });
 
